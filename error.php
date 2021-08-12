c5446b21cd16ccabe6f6b87fdd112517d456fcd1
Jeremy Fleming <jeremyfleming281@gmail.com>
2021-08-12 14:16:33 -0400

    error pages

---
+++ error.php
@@ -0,0 +1,31 @@
+<?php
+
+switch ($_GET[]) {
+
+    case '404':
+        header("Location: /");
+        break;
+    case '401':
+        echo "Access Denied";
+        break;
+    case '403':
+        echo "Access Denied";
+        break;    
+    case '404':
+        header("Location: /");
+        break;  
+    case '400':
+        echo "Bad Request";
+        break;  
+    case '405':
+        echo "Invalid Method";
+        break;
+    case '408':
+        echo "Server Timeout";
+        break;  
+    case '408':
+        echo "Internal Server Error";
+        break;  
+    default:
+        header("Location: /");
+}
